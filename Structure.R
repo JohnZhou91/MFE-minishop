a64be4842d2f44c63a2fcc06a0f6ee4cbd3ac294
JohnZhou91 <Jonathan@wifi-131-179-0-253.host.ucla.edu>
2015-10-30 20:09:28 -0700

    top structure

---
+++ Structure.R
@@ -0,0 +1,38 @@
+############## Initialization ##############
+setwd("~/Desktop/MFE minishop")
+require('data.table')
+require('reshape2')
+require('zoo')
+# require('xlsx')
+
+############## Read data ################
+
+
+############## calcualte factors ##############
+# mean shinkage
+
+############## calculate covariance matrix ##############
+# cov matrix shinkage 
+
+############## transaction cost ##############
+
+
+############## optimize weight ##############
+# Quadratic programming 
+
+############## rebalance ##############
+# Monthly rebalancing 
+
+############## backtest ##############
+
+
+############## Analysis ##############
+# Factor model decomposition 
+
+############## reporting ##############
+# Inlcude today's trades and P&L
+# Cumulative P&L
+# Current total positions 
+# output in csv or xlsx files 
+
+
